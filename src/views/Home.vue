a83bf4f6fd9442681fd87eb7ce64ab3916a966dd
chaudl113 <tnlong1214@gmail.com>
2021-06-09 23:31:47 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home container">
+    <div class="header flex">
+    <div class="left flex flex-cloumn">
+      <h1>Invoices</h1>
+      <span>There are 4 total invoices</span>
+    </div>
+    <div class="rigth flex">
+      <div @click="toggleFilterMenu" class="filter flex" ref="filter">
+        <span>Filler by status</span>
+        <img src="../assets/icon-arrow-down.svg" alt="">
+    </div>
+
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
