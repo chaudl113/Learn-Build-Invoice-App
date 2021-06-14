6ec9d3c4b56bf9392a9bca3d962802e586daebd9
Chaudl113 <tnlong1214@gmail.com>
2021-06-14 15:47:41 +0700

    up

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,26 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory
+} from "vue-router";
 import Home from "../views/Home.vue";
 import InvoiceView from "../views/InvoiceView.vue";
 
 
-const routes = [
-  {
+const routes = [{
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home"
+    }
   },
   {
     path: "/invoice/:invoiceId",
     name: "Invoice",
     component: InvoiceView,
+    meta: {
+      title: "Invoice"
+    }
   },
 ];
 
@@ -20,5 +28,13 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
+
+router.beforeEach(async (to, from, next) => {
+
+  document.title = `${to.meta.title} | Invoice App`
+
+  next()
+})
 
 export default router;
